16cbdb241ad13499614e18e817043a6173949f2d
RAJ <rajkamal11126@gmail.com>
2020-08-30 18:08:15 +0530

    added wages in month

--- EmpWage.java
+++ EmpWage.java
@@ -2,29 +2,30 @@ public class EmpWage
 {
         public static void main(String args[])
         {
-                final int isFullTime=1;
-		final int isPartTime=2;
-                int randomValue=(int)Math.floor(Math.random()*10)%3;
 		int empRatePerHr=20;
 		int salary;
 		int empHrs;
+		int workingDays=20;
+		int totalSalary=0;
+
+		for(int i=0; i<=workingDays; i++){
+		int randomValue=(int)Math.floor(Math.random()*10)%3;
 		switch(randomValue)
 		{
                 case 1:
-                        System.out.println("Employee is FullTime Present");
 			empHrs=8;
 			break;
 
 		case 2:
-			System.out.println("Employee is PartTime Present");
                         empHrs=4;
 			break;
                 default :
-                        System.out.println("Employee is Absent");
 			empHrs=0;
            	}
 		salary=empRatePerHr*empHrs;
-		System.out.println("salary="+salary);
+		totalSalary=totalSalary+salary;
+	}
+		System.out.println("Total Salary="+totalSalary);
         }
 }
 
